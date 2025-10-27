70f42e7a10c2ab739dd691f25b7c982be886e42a
iamsseaa <homingsea@gmail.com>
2025-10-27 21:00:23 +0900

    fix: 입력받는 라이브러리 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,17 +2,14 @@ package racingcar.view;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 import racingcar.validation.InputValidator;
 
 public class InputView {
-
-    private static final Scanner scanner = new Scanner(System.in);
-
     public List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = scanner.nextLine();
+        String input = Console.readLine();
 
         // 예외 처리는 다음 커밋에서 InputValidator로 분리
         return InputValidator.validateCarNames(input);
@@ -20,7 +17,7 @@ public class InputView {
 
     public int readAttemptCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        String input = scanner.nextLine();
+        String input = Console.readLine();
         return InputValidator.validateAttemptCount(input);
     }
 }
